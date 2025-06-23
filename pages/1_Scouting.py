8fe18fec7e82718a4a57ee8d591fa94d57f79c04
Eb3d <114613751+Ebed24@users.noreply.github.com>
2025-06-23 17:02:47 -0600

    Update 1_Scouting.py

--- pages/1_Scouting.py
+++ pages/1_Scouting.py
@@ -6,7 +6,7 @@ st.title('Visualización y Filtros de Jugadores - Scouting')
 
 @st.cache_data
 def load_data():
-    return pd.read_csv('D:\\Mexico FC\\Futbol_Madrid\\Madrid_futbol_2025.csv')
+    return pd.read_csv('Madrid_futbol_2025.csv')
 
 df = load_data()
 
